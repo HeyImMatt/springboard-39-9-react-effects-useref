b5b7ef22f49c56b4794c0538b6b49a3a9c92a0d0
heyimmatt <mrmcfarland@gmail.com>
2020-08-02 13:02:49 -0700

    Add useEffect function to get a deckId on page load

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './CardDeck.css'
 
 function CardDeck() {
+  const [cardsShown, setCardsShown] = useState([]);
+  const [deckId, setDeckId] = useState(null);
+
+  useEffect(() => {
+    async function getDeck() {
+      try {
+        const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        setDeckId(resp.data.deck_id);
+      }
+      catch(err) {
+        alert('Something went wrong')
+        throw new Error(err)
+      }
+    }
+    getDeck();
+  }, [])
+
+  console.log(deckId)
+
   return(
     <div className="CardDeck">
       <button>GET A CARD</button>
