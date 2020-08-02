a53a7480bb19cd2a82669104c4af10ab671276bf
heyimmatt <mrmcfarland@gmail.com>
2020-08-02 15:59:39 -0700

    Add random rotate to each card

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Card from './Card'
 import axios from 'axios'
 import './CardDeck.css'
 
@@ -24,7 +25,7 @@ function CardDeck() {
     if (cardsShown.length < 52) {
       try {
         const resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
-        setCardsShown([...cardsShown, resp.data.cards[0]]);
+        setCardsShown([...cardsShown, {rotate: rotateRandom(), ...resp.data.cards[0]}]);
       }
       catch(err) {
         alert('Something went wrong')
@@ -33,9 +34,15 @@ function CardDeck() {
     } else alert('There are no cards left!')
   }
 
+  const rotateRandom = () => {
+    let n = Math.floor(Math.random() * 360);
+    return `rotate(${n}deg)`
+  }
+
   return(
     <div className="CardDeck">
       <button onClick={getCard}>GET A CARD</button>
+      {cardsShown.map((card, idx) =><Card card={card} idx={idx} />)}
     </div>
   )
 }
